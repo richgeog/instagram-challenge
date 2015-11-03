da5f9f3409f600de9bc44f4c3ff125aaadddd12c
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-11-03 16:16:05 +0000

    added validation for length of title name

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,5 +6,6 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/test.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :title, length: {minimum: 3}
 
 end
