4434af2ac014095fb207a250c867f81fd52cdb7e
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-11-04 22:35:06 +0000

    added user id to comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,8 @@ describe Comment, type: :model do
   context 'relationship with photo' do
     it { is_expected.to belong_to :photo }
   end
+
+  context 'relationshp with the user' do
+    it { is_expected.to belong_to :user }
+  end
 end
