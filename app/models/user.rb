bd616239f65a487ab495bdfb0b73e9b7bfa6e35f
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-11-04 22:47:49 +0000

    added ruser relationships and basic tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 
   has_many :photos
+  has_many :likes
+  has_many :comments
+  has_many :liked_photos, through: :likes, source: :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
