d3f5b92a77759d0f5f9f480797d9099970351ad7
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-03-01 12:13:16 +0000

    refactored photos and likes controllers

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,18 +1,58 @@
 class PhotosController < ApplicationController
-
-  before_action :authenticate_user!, :expcet => [:index, :show]
+  before_action :authenticate_user!, expcet: [:index, :show]
 
   def index
     @photos = Photo.all
   end
 
   def new
-    @user = User.find(current_user.id)
+    @user = user
     @photo = Photo.new
   end
 
+  def show
+    @photo = photo
+  end
+
   def create
-    @user = User.find(current_user.id)
+    @user = user
+    new_photo_created
+  end
+
+  def edit
+    @photo = photo
+    match_current_user_to_photo
+  end
+
+  def update
+    @photo = photo
+    @photo.update(photo_params)
+    redirect_to photos_path
+  end
+
+  def destroy
+    @photo = photo
+    @photo.destroy
+    delete_photo_flash_message
+    redirect_to photos_path
+  end
+
+  private
+
+  def photo_params
+    params[:photo][:user_id] = current_user.id
+    params.require(:photo).permit(:title, :image, :user_id)
+  end
+
+  def user
+    @_user ||= User.find(current_user.id)
+  end
+
+  def photo
+    @_photo ||= Photo.find(params[:id])
+  end
+
+  def new_photo_created
     @photo = Photo.create(photo_params)
     if @photo.save
       redirect_to photos_path
@@ -21,33 +61,13 @@ class PhotosController < ApplicationController
     end
   end
 
-  def show
-    @photo = Photo.find(params[:id])
+  def match_current_user_to_photo
+    return if current_user.id == @photo.user_id
+    flash[:notice] = 'You are unable to edit this photo'
+    redirect_to photos_path
   end
 
-  def edit
-    @photo = Photo.find(params[:id])
-    if current_user.id != @photo.user_id
-      flash[:notice] = 'You are unable to edit this photo'
-      redirect_to photos_path
-    end
-  end
-
-  def update
-    @photo = Photo.find(params[:id])
-    @photo.update(photo_params)
-    redirect_to '/photos'
-  end
-
-  def destroy
-    @photo = Photo.find(params[:id])
-    @photo.destroy
+  def delete_photo_flash_message
     flash[:notice] = 'Photo deleted successfully'
-    redirect_to '/photos'
-  end
-
-  def photo_params
-    params[:photo][:user_id] = current_user.id
-    params.require(:photo).permit(:title, :image, :user_id)
   end
 end
